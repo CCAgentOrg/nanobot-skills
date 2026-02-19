0593732f0bc72189212dba5a53d51c865dde37a8
srikanthlogic <srikanthagentorg@srik.me>
2026-02-19 02:57:42 +0000

    Move lobster-py to untested branch

--- lobster-py/lobster/actions/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Built-in actions for lobster-py workflows.
-
-Provides common actions: echo, shell, sleep, http, etc.
-"""
-
-from lobster.actions.core import Action, ActionRegistry, EchoAction, ShellAction, SleepAction
-
-__all__ = ["Action", "ActionRegistry", "EchoAction", "ShellAction", "SleepAction"]
